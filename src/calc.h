db2f2705431339f0f21e0cdc6459d5b93ae5f54d
Kasmas <kasmas.kosmos@gmail.com>
2018-10-02 15:16:30 +0700

    Update calc.h

--- src/calc.h
+++ src/calc.h
@@ -1,11 +1,6 @@
 #pragma once
-#ifndef CALC_H
-#define CALC_H
-#include "function.c"
 
 int slozhenye(int A, int B);
 int vichitanye(int A, int B);
 int umnozhenye(int A, int B);
 int delenye(int A, int B);
-	
-#endif
