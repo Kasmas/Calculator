8938d32e7204111143de27f62e5d97d431332f92
Kasmas <kasmas.kosmos@gmail.com>
2018-10-02 15:06:56 +0700

    Update calc.h

--- src/calc.h
+++ src/calc.h
@@ -1,24 +1,9 @@
 #ifndef CALC_H
 #define CALC_H
 
-int slozhenye(int A, int B) {
-	return A + B;
-}
-
-int vichitanye(int A, int B) {
-	return A - B;
-}
-
-int umnozhenye(int A, int B) {
-	return A * B;
-}
-
-int delenye(int A, int B) {
-	if (B == 0) {
-		return A;
-	}
-
-	return A / B;
-}
-
+int slozhenye(int A, int B);
+int vichitanye(int A, int B);
+int umnozhenye(int A, int B);
+int delenye(int A, int B);
+	
 #endif
