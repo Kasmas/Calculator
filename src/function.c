fad21b1ca06a8aabd96d25e2b4dd6a28b5787961
Kasmas <kasmas.kosmos@gmail.com>
2018-10-02 15:10:14 +0700

    Create function.c

---
+++ src/function.c
@@ -0,0 +1,22 @@
+#include "calc.h"
+#include <math.h>
+
+int slozhenye(int A, int B) {
+  return A + B;
+}
+ 
+ int vichitanye(int A, int B) {
+  return A - B;
+}
+ 
+ int umnozhenye(int A, int B) {
+	return A * B;
+}
+ 
+ int delenye(int A, int B) {
+	if (B == 0) 
+  {
+		return A;
+	}
+ return A / B;
+}
